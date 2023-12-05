756ae91f617b85b1802678c9800436affff3a47c
Grace Zhang <gracewzhang01@gmail.com>
2023-12-04 22:52:34 -0600

    Add rich.console

--- src/cli.py
+++ src/cli.py
@@ -1,29 +1,33 @@
 import sys
+from rich.console import Console
+
+console = Console()
 
 
 class CLI():
     def __init__(self) -> None:
         self.special_cmds = {
-            'h;': self.print_help_msg,
-            'e;': self.exit_sapphire,
+            'h;': self.__print_help_msg,
+            'e;': self.__exit_sapphire,
         }
-        self.print_start_msg()
+        self.__print_start_msg()
 
-    def print_start_msg(self) -> None:
+    def __print_start_msg(self) -> None:
         start_msg = '=== Welcome! Type in your desired command, and press enter when you\'re done ===\n' \
             + ' \'h;\': help menu\n' \
             + ' \'e;\': exit'
-        print(start_msg)
+        console.print(start_msg)
 
-    def print_help_msg(self) -> None:
+    def __print_help_msg(self) -> None:
         help_msg = '[insert help msg]'
-        print(help_msg)
+        console.print(help_msg)
 
     def get_user_input(self) -> str:
         cmd = input()
         if cmd in self.special_cmds:
             self.special_cmds[cmd]()
+            return None
         return cmd
 
-    def exit_sapphire(self) -> None:
+    def __exit_sapphire(self) -> None:
         sys.exit(0)
--- src/sapphire.py
+++ src/sapphire.py
@@ -1,25 +1,28 @@
 from openai import OpenAI
+from cli import console
 import os
 import sys
 import subprocess
+
 
 class Sapphire():
     def __init__(self) -> None:
         self.__setup_connection()
         self.__detect_system()
-        self.msgs = [{'role': 'system', 'content':  
-              'You are an intelligent assistant named Sapphire that helps the \
-              user execute command line commands to manage their desktop'}]
+        self.msgs = [{'role': 'system', 'content':
+                      'You are an intelligent assistant named Sapphire that helps the \
+                      user execute command line commands to manage their desktop'}]
 
     def __setup_connection(self) -> None:
         api_key = os.getenv('OPENAI_API_KEY')
         if api_key is None:
-            print('Make sure to add your OpenAI API KEY to your system environment as OPENAI_API_KEY.')
+            console.print(
+                'Make sure to add your OpenAI API KEY to your system environment as OPENAI_API_KEY.')
             sys.exit(0)
         self.client = OpenAI(api_key=api_key)
 
     def __detect_system(self) -> None:
-        platform = sys.platform 
+        platform = sys.platform
         if platform.startswith('linux'):
             self.system = 'Linux'
         elif platform.startswith('darwin'):
@@ -27,34 +30,34 @@ class Sapphire():
         elif platform .startswith('win32'):
             self.system = 'Windows'
         else:
-            print('User platform is incompatible with Sapphire :(')
+            console.print('User platform is incompatible with Sapphire :(')
             sys.exit(0)
 
     def execute_cmd(self, user_cmd) -> None:
         sapphire_cmd = self.__get_sapphire_cmd(user_cmd)
-        if sapphire_cmd == 'Invalid command':
-            print('Please enter a valid command.')
+        if sapphire_cmd.startswith('Invalid command'):
+            console.print('Please enter a valid command.')
         elif self.__greenlight_sapphire_cmd(sapphire_cmd):
             subprocess.call(sapphire_cmd, shell=True)
         else:
-            print('=== Aborting ===')
+            console.print('=== Aborting ===')
 
     def __greenlight_sapphire_cmd(self, sapphire_cmd) -> bool:
-        print(f'Executing {sapphire_cmd}... [Y/N]')
+        console.print(f'Executing {sapphire_cmd}... [Y/N]')
         res = input()
         if res == 'Y' or res == 'y':
-            return True 
+            return True
         return False
 
     def __get_sapphire_cmd(self, user_cmd) -> str:
         cmd = f'What is the script in a {self.system} environment to execute the following command:' \
             + f'"{user_cmd}". Reply with just the command and no additional text.' \
-            + f'If it is not possible to answer with a command line command, reply with' \
-            + f'"Invalid command"'
+            + 'If it is not possible to answer with a command line command, reply with' \
+            + '"Invalid command"'
         self.msgs.append({'role': 'user', 'content': cmd})
-        chat = self.client.chat.completions.create( 
-            model="gpt-3.5-turbo", messages=self.msgs 
-        ) 
-        reply = chat.choices[0].message.content 
-        self.msgs.append({'role': 'assistant', 'content': reply}) 
-        return reply 
+        chat = self.client.chat.completions.create(
+            model="gpt-3.5-turbo", messages=self.msgs
+        )
+        reply = chat.choices[0].message.content
+        self.msgs.append({'role': 'assistant', 'content': reply})
+        return reply
